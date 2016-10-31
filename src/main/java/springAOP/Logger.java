481d9e868bd394256cab2ede15679d1352e70781
songi3 <wks4j1004@gmail.com>
2016-11-01 01:18:23 +0900

    annotation 활용한 AOP

--- src/main/java/springAOP/Logger.java
+++ src/main/java/springAOP/Logger.java
@@ -1,8 +1,23 @@
 package springAOP;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class Logger {
 
-	public void aboutToSound(){
+	@Pointcut("execution(void springAOP.*.sound())") //expression
+	
+	private void selectSound(){//signature //xml 파일 안에 id에 해당
+		
+	}
+	
+	//런타임 시에 Before Advice가 실행 -> logging, security, tansaction을 본 애플리케이션의 기능에 영향을 주지않고 실행할수 있다
+	//코드 깔끔 , 재사용성 높아짐
+	
+	@Before("selectSound()")
+	public void aboutToSound() {
 		System.out.println("before : about to sound");
-	}//advice
-}//aspect
+	}// advice
+}// aspect
--- src/main/java/springAOP/PetOwner.java
+++ src/main/java/springAOP/PetOwner.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 public class PetOwner {
 
 	@Autowired //wiring by type
-	@Qualifier(value="qf_dog")
+	@Qualifier(value="qf_cat")
 	
 	private AnimalType animal;
 
